9966afe125f979fc18964656375a29d1a8817f8c
zhilyaevaviktorija <zhilyaevaviktorija@rambler.ru>
2025-10-01 13:22:41 +0300

    refactor: add task 3

---
+++ Task3.py
@@ -0,0 +1,18 @@
+greet_text = "Hello, world!"
+print(greet_text)
+
+# 1. Заменяю текст в переменной greet_text
+greet_text = "Hey"
+print(greet_text)
+
+# 2. Создаю новую переменную new_text
+new_text = "Today"
+print(new_text)
+
+# 3. Записываю new_text в greet_text
+greet_text = new_text
+print(greet_text)
+
+# 4. Изменяю свойства new_text
+new_text += " Wow!"
+print(new_text)
