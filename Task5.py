9d97026543f6ea420a69c8ee0dff774d02a605d9
zhilyaevaviktorija <zhilyaevaviktorija@rambler.ru>
2025-10-01 13:48:11 +0300

    refactor: add task 4

---
+++ Task5.py
@@ -0,0 +1,22 @@
+# Создала переменную с приветствием
+greet_text = "It's a greeting!"
+print(greet_text)
+
+# Добавила переменную name
+name = "Anna"
+print("Hi, " + name)
+
+# Сделала конкатенацию строк
+name = "Anna"
+print("Hi, " + name + "!")
+
+# Добавила переменную surname,
+# Сделала конкатенацию строк
+name = "Anna"
+surname = ' ' + "Smirnova"
+name += surname
+print("Hi, " + name + "!")
+
+# Вывела полную информацию пользователя
+age = ' ' + "25"
+print("User:", name, "\n", "Age:" + age)
