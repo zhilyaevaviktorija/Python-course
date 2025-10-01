9400e0de69e5cc852d131839ce83b972858e2d1b
zhilyaevaviktorija <zhilyaevaviktorija@rambler.ru>
2025-10-01 13:34:28 +0300

    refactor: add task 4

---
+++ Task4.py
@@ -0,0 +1,10 @@
+def main(greet_text):
+    # Вместо pass написала строку кода для вывода на экран приветствия
+    print(greet_text)
+
+
+if __name__ == "__main__":
+    # Произвела присваивание переменной
+    # Вместо None написала Hi
+    greet_text = "Hi"
+    main(greet_text)
